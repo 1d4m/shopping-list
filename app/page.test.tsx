d4d425b468a88ec26269d858a7337a78f8b69ac7
daxchx <daxchx.v1@gmail.com>
2025-10-18 17:36:13 +0900

    delete: 不要テストファイルを削除

--- app/page.test.tsx
+++
@@ -1,8 +0,0 @@
-import { test } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import Home from './page';
-
-test('Page', () => {
-  render(<Home />);
-  screen.getByText('買い物リスト');
-});
